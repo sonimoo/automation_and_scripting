ab76660a59f31ef0741f9a613868ed38382876c0
Sofia <kalinkovasonea@gmail.com>
2025-12-03 23:11:11 +0200

    Fix deploy pipeline -_-

--- lab05/pipelines/php_deploy_pipeline.groovy
+++ lab05/pipelines/php_deploy_pipeline.groovy
@@ -3,17 +3,17 @@ pipeline {
 
     environment {
         PHP_REPO = "https://github.com/sonimoo/project_for_lab04.git"
+        PHP_APP_DIR = "php-app"
     }
 
     stages {
-
         stage('Clone PHP Project') {
             steps {
                 echo "Клонируем репозиторий PHP проекта..."
-                sh """
-                rm -rf php-app
-                git clone ${PHP_REPO} php-app
-                """
+                sh '''
+                    rm -rf ${PHP_APP_DIR}
+                    git clone ${PHP_REPO} ${PHP_APP_DIR}
+                '''
             }
         }
 
@@ -21,10 +21,10 @@ pipeline {
             steps {
                 echo "Размещаем проект на тестовом сервере через Ansible..."
 
-                sshagent(credentials: ['ssh-agent-key']) {
+                sshagent(credentials: ['jenkins']) {
                     sh '''
-                        ssh -o StrictHostKeyChecking=no jenkins@ssh-agent "
-                            cd /home/jenkins/ansible &&
+                        ssh -o StrictHostKeyChecking=no ansible@ansible-agent "
+                            cd /home/ansible/ansible &&
                             ansible-playbook -i hosts.ini deploy_php_app.yml
                         "
                     '''
@@ -34,11 +34,11 @@ pipeline {
     }
 
     post {
-        success {
-            echo "PHP проект успешно размещён на тестовом сервере!"
-        }
         failure {
             echo "Ошибка при размещении PHP проекта!"
+        }
+        success {
+            echo "PHP проект успешно развернут на тестовом сервере!"
         }
     }
 }
