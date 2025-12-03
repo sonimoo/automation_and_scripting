aa829a4e0a5893ccc143ce027b5659ddeecd13b8
Sofia <kalinkovasonea@gmail.com>
2025-12-03 22:57:28 +0200

    Add PHP deploy pipeline and Ansible deployment playbook

---
+++ lab05/pipelines/php_deploy_pipeline.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+    environment {
+        PHP_REPO = "https://github.com/sonimoo/project_for_lab04.git"
+    }
+
+    stages {
+
+        stage('Clone PHP Project') {
+            steps {
+                echo "Клонируем репозиторий PHP проекта..."
+
+                sh """
+                rm -rf php-app
+                git clone ${PHP_REPO} php-app
+                """
+            }
+        }
+
+        stage('Deploy to Test Server with Ansible') {
+            steps {
+                echo "Размещаем проект на тестовом сервере через Ansible..."
+
+                sh """
+                cd ansible
+                ansible-playbook -i hosts.ini deploy_php_app.yml
+                """
+            }
+        }
+    }
+
+    post {
+        success {
+            echo "PHP проект успешно размещён на тестовом сервере!"
+        }
+        failure {
+            echo "Ошибка при размещении PHP проекта!"
+        }
+    }
+}
